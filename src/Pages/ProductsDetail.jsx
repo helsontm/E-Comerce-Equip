136aba1667caa32f1f28795768d0d1dab05ffba4
Helson Tafur Manchego <helsontm@hotmail.com>
2023-10-04 00:09:41 -0500

    actualizacion

--- src/Pages/ProductsDetail.jsx
+++ src/Pages/ProductsDetail.jsx
@@ -27,7 +27,7 @@ const ProductsDetail = () => {
      const getDetail = () => {
         axios
         //.get( `https://e-commerce-api-v2.academlo.tech/api/v1/products/${id}`)
-        .get(`http://localhost:8080/product/${id}`)
+        .get(`https://ecommerce-app-ktdy.onrender.com/product/${id}`)
         .then(resp => { setProductDetail(resp?.data);
          dispatch(filterProductsByCategoryThunk(resp?.data?.category?.id))})
         .catch(error => console.error(error))
